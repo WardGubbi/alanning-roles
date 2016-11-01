8a9ac161208e66b4544cd40a6ba4d00d9266948a
Petr Janu <janu@cognito.cz>
2016-11-01 16:55:36 +0100

    Update typings

--- main.d.ts
+++ main.d.ts
@@ -9,14 +9,17 @@ interface RolesDAO {
 }
 
 declare module Roles {
+
   function createRole(roleName: string): string;
   function deleteRole(roleName: string): void;
-  function addUsersToRoles(users: any, roles: any, groups?: string): void;
-  function removeUsersFromRoles(users: any, roles: any): void;
-   // User can be user ID or user object.
-  function userIsInRole(user: any, roles: any): boolean;
-  function getRolesForUser(userId: string): string[];
+  function addUsersToRoles(users: any, roles: string | Array<string>, group?: string): void;
+  function setUserRoles(users: any, roles: string | Array<string>, group?: string): void;
+  function removeUsersFromRoles(users: any, roles: string | Array<string>, group?: string): void;
+  function userIsInRole(user: any, roles: string | Array<string>, group?: string): boolean;
+  function getRolesForUser(userId: any): string[];
   function getAllRoles(): Mongo.Cursor<RolesDAO>;
-  function getUsersInRole(roleName: string): Mongo.Cursor<RolesDAO>;
+  function getUsersInRole(role: any, group?: string, options?: any): Mongo.Cursor<RolesDAO>;
+  function getGroupsForUser: function (user: any, role?: string): Array<any>;
+
   var GLOBAL_GROUP: string;
 }
