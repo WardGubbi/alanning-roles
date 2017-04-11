8555969a56615b1a93e928d6bbd3c1416afde315
Ward Gubbi <WardGubbi@users.noreply.github.com>
2017-04-11 13:03:05 +0200

    declare module

--- main.d.ts
+++ main.d.ts
@@ -2,24 +2,25 @@
 // Project: https://atmospherejs.com/alanning/roles
 // Definitions by:
 // Dave Allen <https://github.com/fullflavedave>
+declare module "meteor/alanning:roles" {
+  interface RolesDAO {
+    _id?: string;
+    name?: string;
+  }
+  
+  module Roles {
+  
+    function createRole(roleName: string): string;
+    function deleteRole(roleName: string): void;
+    function addUsersToRoles(users: any, roles: string | Array<string>, group?: string): void;
+    function setUserRoles(users: any, roles: string | Array<string>, group?: string): void;
+    function removeUsersFromRoles(users: any, roles: string | Array<string>, group?: string): void;
+    function userIsInRole(user: any, roles: string | Array<string>, group?: string): boolean;
+    function getRolesForUser(userId: any): string[];
+    function getAllRoles(): Mongo.Cursor<RolesDAO>;
+    function getUsersInRole(role: any, group?: string, options?: any): Mongo.Cursor<RolesDAO>;
+    function getGroupsForUser(user: any, role?: string): Array<any>;
 
-interface RolesDAO {
-  _id?: string;
-  name?: string;
-}
-
-declare module Roles {
-
-  function createRole(roleName: string): string;
-  function deleteRole(roleName: string): void;
-  function addUsersToRoles(users: any, roles: string | Array<string>, group?: string): void;
-  function setUserRoles(users: any, roles: string | Array<string>, group?: string): void;
-  function removeUsersFromRoles(users: any, roles: string | Array<string>, group?: string): void;
-  function userIsInRole(user: any, roles: string | Array<string>, group?: string): boolean;
-  function getRolesForUser(userId: any): string[];
-  function getAllRoles(): Mongo.Cursor<RolesDAO>;
-  function getUsersInRole(role: any, group?: string, options?: any): Mongo.Cursor<RolesDAO>;
-  function getGroupsForUser(user: any, role?: string): Array<any>;
-
-  var GLOBAL_GROUP: string;
+    var GLOBAL_GROUP: string;
+  }
 }
